12f6206b7c001e6e8d67d820dfcf0e2e461d8ee9
Nane <gemine_nicolas@hotmail.com>
2025-03-24 00:50:31 +0100

    23-03-25 React 18b-281 END

--- src/components/Opinion.jsx
+++ src/components/Opinion.jsx
@@ -1,18 +1,26 @@
-import {use} from "react";
+import {use, useActionState, useOptimistic} from "react";
 import {OpinionsContext} from "../store/opinions-context.jsx";
 
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
   const {upvoteOpinion, downvoteOpinion} = use(OpinionsContext);
 
+  const [optimisticVotes, setVotesOptimistically] = useOptimistic(
+      votes,
+      (prevVotes, mode) => (mode === 'up' ? prevVotes + 1 : prevVotes - 1)
+  );
+
   async function upVoteAction() {
+    setVotesOptimistically('up');
     await upvoteOpinion(id);
-    console.log('UPVOTE');
   }
 
   async function downVoteAction() {
+    setVotesOptimistically('down');
     await downvoteOpinion(id);
-    console.log('DOWNVOTE');
   }
+
+  const [upvoteFormState, upvoteFormAction, upvotePending] = useActionState(upVoteAction);
+  const [downvoteFormState, downvoteFormAction, downvotePending] = useActionState(downVoteAction);
 
   return (
     <article>
@@ -22,7 +30,10 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
       </header>
       <p>{body}</p>
       <form className="votes">
-        <button formAction={upVoteAction}>
+        <button
+            formAction={upvoteFormAction}
+            disabled={upvotePending || downvotePending}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -40,9 +51,12 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
           </svg>
         </button>
 
-        <span>{votes}</span>
+        <span>{optimisticVotes}</span>
 
-        <button formAction={downVoteAction}>
+        <button
+            formAction={downvoteFormAction}
+            disabled={upvotePending || downvotePending}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
